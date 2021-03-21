f871dc2a7d4dc141fd796f4406912c16d9847916
RAVIKUMARBALIJA <bravikumar123@hotmail.com>
2021-03-21 10:00:04 +0530

    app

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request, jsonify
 import os
 import yaml
 import joblib
 import numpy as np
-import pandas as pd
 
 params_path = "params.yaml"
 webapp_root = "webapp"
 
-static_dir = os.path.join(webapp_root,"static")
-template_dir = os.path.join(webapp_root,"templates")
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-@app.route("/",method=["GET","POST"])
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
-if __name__ == "__main__":
-    app.run()
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response":response}
+        print(response)
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again"}
+        return error
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float, data))]
+                response = predict(data)
+                return render_template("index.html", response=response)
+
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again"}
+            return render_template("404.html", error=error)
+    else:
+        return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host="localhost", port=5000, debug=True)
